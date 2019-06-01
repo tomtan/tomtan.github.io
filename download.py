43c7827060cff89d7799218418bf0c5191605780
tomtan <tomyt.tan@gmail.com>
2019-06-01 09:09:55 +0800

    '20190601'

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import urllib.request
+from urllib.error import URLError, HTTPError
 import json
 import os
 
@@ -11,16 +12,31 @@ def open_url(url):
 
     return response
 
+def record(recordChapterId):
+    recordFileName = 'update.txt'
+    recordFile = open(recordFileName, 'w')
+    recordFile.write(recordChapterId)
+    recordFile.close()
+    print('记录完毕! 当前章节为： ' + recordChapterId)
+
+def updateContent(fname, chapterName):
+    with open('content.html', 'a+', encoding='utf-8') as contentFile:
+        # 写入目录
+        contentFile.write('<a target="_blank" href="' + fname + '">' + chapterName + '</a><br/>')
+    
 def main():
     result = None
     # https://m.whzh-cw.com/du/66660/
     # https://m.whzh-cw.com/files/66/66660/66660.json?cdnversions=5195519
     
     json_url = 'https://m.whzh-cw.com/files/66/66660/66660.json?cdnversions=5195519'
-
-    json_body = open_url(json_url)
-    # json_str = json_body.read().decode('utf8')
-    result = json.load(json_body)
+    try:
+        json_body = open_url(json_url)
+        # json_str = json_body.read().decode('utf8')
+        result = json.load(json_body)
+    except Exception as e:
+        print(e)
+        return;
     
     '''
     with open('c:/r4r/66660.json') as src:
@@ -53,98 +69,105 @@ def main():
                 print('记录为空！')
                 return
             elif record.isdigit():    
-                recordChapterId = int(record)
+                recordChapterId = record
             else:
                 print('出现异常！')
                 return
 
-    print('>>>记录章节ID：' + str(recordChapterId))
+    print('>>>记录章节ID：' + recordChapterId)
     
-    if recordChapterId >= latestChapterNum:
+    if int(recordChapterId) >= latestChapterNum:
         print('已更新至最新章节！')
         return
+    
+    for each in result['list']:
+        chapterId = each['chapterid']
+        chapterName = each['chaptername']
 
-    with open('content.html', 'a+', encoding='utf-8') as contentFile:
+        if int(chapterId) <= int(recordChapterId):  # 忽略这些内容的下载
+            continue
 
-        for each in result['list']:
-            chapterId = each['chapterid']
-            chapterName = each['chaptername']
+        print('>>>' + chapterName)
+        print('>>>' + chapterId)
+        
+        #https://m.whzh-cw.com/files/article/html555/70/70538/
+        url = 'https://m.whzh-cw.com/files/article/html555/70/' + str(result['info']['articleid']) + '/' + str(chapterId) + '.html'
+        
+        body = None
 
-            if int(chapterId) <= recordChapterId:  # 忽略这些内容的下载
-                continue
-
-            print('>>>' + chapterName)
-            
-            #https://m.whzh-cw.com/files/article/html555/70/70538/
-            url = 'https://m.whzh-cw.com/files/article/html555/70/' + str(result['info']['articleid']) + '/' + str(chapterId) + '.html'
-
+        try:
             body = open_url(url)
-            html = body.read().decode('gbk')
+        except HTTPError or UTLError or Exception:
+            print('请求异常！')
+            break
+        
+        html = body.read().decode('gbk')
 
-            if html.startswith('var _0x') or html.startswith('var cctxt='):
-                print('数据被加密，需要JavaScript解码')
+        if html.startswith('var _0x') or html.startswith('var cctxt='):
+            print('数据被加密，需要JavaScript解码')
+            
+            scriptName = chapterId + '.script.html'
+            with open(scriptName, 'w', encoding='utf-8') as f:
+                f.write(html)
+
+            fname = chapterId + '.html'
+            with open(fname, 'w', encoding='gbk') as f:
+                content = '''<!DOCTYPE html>
+                            <html>
+                            <head>
+                                <meta http-equiv="Content-Type" content="text/html; charset=gbk" />
+
+                                <script src="./jquery-1.8.1.min.js"></script>
+                            </head>
+                            <body>
+                                <div id="content"></div>
+                            </body>
+                            <script type="text/javascript">
+                                $.ajax({
+                                    type: "GET",
+                                    url: '%s',
+                                    dataType: "script",
+                                    cache: true,
+                                    success: function(msg){
+                                        msg=cctxt;
+
+                                        $('#content').html(msg);
+                                    }
+                                });
+
+                            </script>
+                            </html>
+                        '''
+                f.write(content % ('https://tomtan.github.io/' + scriptName))
+
+            # 写入目录
+            updateContent(fname, chapterName)
+            print('>>', url)
                 
-                scriptName = chapterId + '.script.html'
-                with open(scriptName, 'w', encoding='utf-8') as f:
-                    f.write(html)
-
-                fname = chapterId + '.html'
-                with open(fname, 'w', encoding='gbk') as f:
-                    content = '''<!DOCTYPE html>
-                                <html>
-                                <head>
-                                    <meta http-equiv="Content-Type" content="text/html; charset=gbk" />
-
-                                    <script src="./jquery-1.8.1.min.js"></script>
-                                </head>
-                                <body>
-                                    <div id="content"></div>
-                                </body>
-                                <script type="text/javascript">
-                                    $.ajax({
-                                        type: "GET",
-                                        url: '%s',
-                                        dataType: "script",
-                                        cache: true,
-                                        success: function(msg){
-                                            msg=cctxt;
-
-                                            $('#content').html(msg);
-                                        }
-                                    });
-
-                                </script>
-                                </html>
-                            '''
-                    f.write(content % ('https://tomtan.github.io/' + scriptName))
-
-                # 写入目录
-                contentFile.write('<a target="_blank" href="' + fname + '">' + chapterName + '</a><br/>')
-                print('>>', url)
-                    
-            else:
-                fname = chapterId + '.html'
-                with open(fname, 'w', encoding='utf-8') as f:
-                    html=html.replace("茵右脚楞夺",'的')
-                    html=html.replace("茵右脚楞夺",'的')
-                    html=html.replace("顺困顶枯枵",'是')
-                    html=html.replace("顶置中夺粗功肖功地",'有')
-                    html=html.replace("夺回顾功带困顺另",'一')
-                    html=html.replace("夺回顾功带困", '有')
-                    html=html.replace("顶置中夺粗功肖功地",'的')
-                    html=html.replace("茵右脚楞夺",'是')
-                    html=html.replace("夺回顾功带困",'有')
-                    html=html.replace("顺困顶枯枵",'，')
-                
-                    f.write(html)
-                # 写入目录
-                contentFile.write('<a target="_blank" href="' + fname + '">' + chapterName + '</a><br/>')
-                print('>>', url)
-
-            recordChapterId = int(chapterId)
-    recordFile = open(recordFileName, 'w')
-    recordFile.write(str(recordChapterId))
-    recordFile.close()
+        else:
+            fname = chapterId + '.html'
+            with open(fname, 'w', encoding='utf-8') as f:
+                html=html.replace("茵右脚楞夺",'的')
+                html=html.replace("茵右脚楞夺",'的')
+                html=html.replace("顺困顶枯枵",'是')
+                html=html.replace("顶置中夺粗功肖功地",'有')
+                html=html.replace("夺回顾功带困顺另",'一')
+                html=html.replace("夺回顾功带困", '有')
+                html=html.replace("顶置中夺粗功肖功地",'的')
+                html=html.replace("茵右脚楞夺",'是')
+                html=html.replace("夺回顾功带困",'有')
+                html=html.replace("顺困顶枯枵",'，')
+            
+                f.write(html)
+            # 写入目录
+            updateContent(fname, chapterName)
+            print('>>', url)
+        # 保存需要记录到文件中章节ID
+        recordChapterId = chapterId
+        
+        
+    # 更新目录并执行完毕
+    record(recordChapterId)
     print('已执行完毕！')
             
         
